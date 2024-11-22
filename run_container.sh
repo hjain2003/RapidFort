4a40bb509dd3fda70095df9dabeaf5177b6d6ff6
Harsh <hjain_be21@thapar.edu>
2024-11-23 04:05:17 +0530

    added bash script

---
+++ run_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Pulling the latest Docker image..."
+docker pull hjain2003/rapidfortserver:latest
+
+echo "Stopping and removing any existing container..."
+docker stop rapidfortserver || echo "No container to stop."
+docker rm rapidfortserver || echo "No container to remove."
+
+echo "Running the Docker container..."
+docker run -d -p 5000:5000 --name rapidfortserver hjain2003/rapidfortserver:latest
+
+echo "Container is now running on port 5000."
